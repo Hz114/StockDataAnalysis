633d7c1328f7fcdf08409e8bd87538a3c503fe1e
Hz114 <kauphj17@gmail.com>
2021-07-02 14:33:47 +0900

    Add yahoo finance API

--- StockDataAnalysisApp/views.py
+++ StockDataAnalysisApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.views.generic import View
 
 # Create your views here.
 def main(request):
--- yahooFinanceData.py
+++ yahooFinanceData.py
@@ -1,7 +1,16 @@
 import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+from yahoo_finance import Share
+import yfinance as yf
+
+'''
+import datetime
 import pandas_datareader as pdr
 from pandas_datareader import data
-
 # 종목 타입에 따라 download url이 다름. 종목코드 뒤에 .KS .KQ등이 입력되어야해서 Download Link 구분 필요
 stock_type = {
     'kospi': 'stockMkt',
@@ -50,28 +59,51 @@ def getAllCode():
     # data frame title 변경 '회사명' = name, 종목코드 = 'code'
     code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
-    print(code_df)
     return code_df
 
 def yahooFinanceData(name):
     code_df = getAllCode()
     code = getCode(code_df, name)
+    # df = pdr.get_data_yahoo(code)
 
-    code = getCode(code_df, name)
-    '''
-    df = pdr.get_data_yahoo(code)
-
-    '''
     # 수정 주기를 반영한 코드
     # 수정주가를 반영
-    '''
     df = pdr.DataReader(code, adjust_price=True)
     df['Close'].plot(figsize=(10, 5))
     print(df['Close'].plot())
-    '''
+
     # Open / High / Low / Close / Volume / Adj_Ratio
-    df = data.YahooDailyReader(code, '2010-07-04', '2016-07-08', adjust_price=True).read()
-    print(df)
+    df = data.YahooDailyReader('NWAU', '2010-07-04', '2016-07-08', adjust_price=True)
+
+    #df = data.get_data_yahoo(code)
+    print(df.read())
+'''
 
 if __name__ == '__main__':
-    yahooFinanceData('삼성전자')
+    # yahooFinanceData('삼성전자')
+    '''
+    df = pdr.get_data_yahoo('NWAU')
+    print(df['Close'].plot())
+    '''
+    #data = yf.download(['NWAU','MU', 'AMD'],start = '2019-01-01')
+    #print(data)
+
+    list = ["NWAU", "MU","AMD","SPY","AAPL"]
+
+    for idx in range(len(list)):
+        data = DataFrame(yf.download(list[idx], start="2021-06-01", end="2021-07-02"))
+        print(data.columns) # 각 값
+        print(data.index) # 날짜 데이터
+
+        data['Adj Close'].plot()
+        plt.xlabel("time")
+        plt.ylabel("money")
+        plt.show()
+
+        #data['Adj Close'].plot().show()
+        '''
+        x = np.arange(1, 10)
+        y = x * 5
+        plt.plot(x, y)
+        # print(plt.show())
+        '''
